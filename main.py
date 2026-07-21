3d79e938188f92bda7104a12121dcbdc72cbb75c
g2683026 <g2683026@tcu.ac.jp>
2026-07-22 04:30:58 +0900

    enable cors for base44

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI, HTTPException
+from fastapi.middleware.cors import CORSMiddleware
 
 import osmnx as ox
 import networkx as nx
@@ -11,6 +12,14 @@ from pyproj import Transformer
 import json
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.get("/")
 def root():
